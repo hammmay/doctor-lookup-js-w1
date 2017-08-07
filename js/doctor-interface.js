609fed70390106634f6a80e8238e623e01ba7962
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-07 16:08:30 -0700

    Add promises to control the order of the api request

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,9 +6,10 @@ $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(JSON.stringify(response));
-    });
-  });
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
+         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      });
+   });
 });
