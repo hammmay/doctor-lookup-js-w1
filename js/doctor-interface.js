ae248f09874d5851c72fbb057719d0bb4dc6547b
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-07 16:37:07 -0700

    Add fail for unfulfilled api request

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -8,8 +8,12 @@ $(document).ready(function() {
     $('#location').val("");
 
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
-     .then(function(response) {
-         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     .then(function(response){
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     })
+
+     .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
       });
-   });
+  });
 });
